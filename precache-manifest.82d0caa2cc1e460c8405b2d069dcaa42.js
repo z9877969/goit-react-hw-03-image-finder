448e58d1798eff40eebd31a88fbef669904272f4
Andriy Kyrylenko <z9877969@ukr.net>
2019-10-30 01:55:36 +0200

    Updates

---
+++ precache-manifest.82d0caa2cc1e460c8405b2d069dcaa42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2de9b61371487cb0c9ffb295e7190ab",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "718689917b3da95af1bf",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.41394553.chunk.css"
+  },
+  {
+    "revision": "7956df21495c36c854a6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.244db1a9.chunk.js"
+  },
+  {
+    "revision": "718689917b3da95af1bf",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.5b822d24.chunk.js"
+  },
+  {
+    "revision": "aca65fd366c2e0de62e7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.1e7696ef.js"
+  }
+]);
